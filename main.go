db3bbb2e7619ef146a59af8f197f35078aaa9a26
bingoohuang <bingoo.huang@gmail.com>
2021-07-20 10:20:36 +0800

    add samples

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 const port = 8080
 
 //go:embed index.html
-var staticTemplates embed.FS
+var indexHtml embed.FS
 
 // ErrorLevel is the type of error found
 type ErrorLevel string
@@ -53,123 +53,152 @@ func getText(r *http.Request) (string, error) {
 	}
 	defer file.Close()
 	var buf bytes.Buffer
-	defer buf.Reset()
 	io.Copy(&buf, file)
 	return buf.String(), nil
 }
 
 func main() {
-	funcs := htmlTemplate.FuncMap{
-		"intRange": func(start, end int) []int {
-			n := end - start + 1
-			result := make([]int, n)
-			for i := 0; i < n; i++ {
-				result[i] = start + i
-			}
-			return result
-		},
-		"nl": func() string {
-			return "\n"
-		},
-		"split": func(str string) []string {
-			return strings.Split(str, ": ")
-		},
+	fns := htmlTemplate.FuncMap{
+		"intRange": intRange,
+		"nl":       nl,
+		"split":    split,
 	}
-	indexTemplate, err := htmlTemplate.New("index.html").Funcs(funcs).ParseFS(staticTemplates, "*")
+	index, err := htmlTemplate.New("index.html").Funcs(fns).ParseFS(indexHtml, "*")
 	if err != nil {
 		panic(err)
 	}
 
 	r := chi.NewRouter()
-
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-		maxRequestSize := int64(32 << 20)
-		r.ParseMultipartForm(maxRequestSize)
-
-		tplErrs := []templateError{}
-
-		text, err := getText(r)
-		if err == http.ErrMissingFile {
-			tplErrs = append(tplErrs, templateError{
-				Line:        -1,
-				Char:        -1,
-				Description: "couldn't accept file",
-			})
-		} else if err != nil {
-			panic(err)
-		}
-
-		var data interface{}
-		rawData := r.FormValue("data")
-		if rawData != "" {
-			err = json.Unmarshal([]byte(rawData), &data)
-			if err != nil {
-				tplErrs = append(tplErrs, templateError{
-					Line:        -1,
-					Char:        -1,
-					Description: fmt.Sprintf("failed to understand data: %v", err),
-					Level:       misunderstoodError,
-				})
-			}
-		}
-
-		t := textTemplate.New("input template")
-
-		// mock template functions - this'll happen automatically as they're found, but errors will be output and there's a max limit
-		rawFunctions := r.FormValue("functions")
-		var functions []string
-		if rawFunctions != "" {
-			functions = strings.Split(rawFunctions, ",")
-		}
-		for _, function := range functions {
-			functionName := strings.TrimSpace(function)
-			// wrap in func so we can catch panics on bad function names
-			func() {
-				defer func() {
-					if r := recover(); r != nil {
-						tplErrs = append(tplErrs, templateError{
-							Line:        -1,
-							Char:        -1,
-							Description: fmt.Sprintf(`bad function name provided: "%s"`, functionName),
-							Level:       misunderstoodError,
-						})
-					}
-				}()
-				t = t.Funcs(textTemplate.FuncMap{functionName: func() error { return nil }})
-			}()
-		}
-
-		parsedT, parseTplErrs := parse(text, t)
-		tplErrs = append(tplErrs, parseTplErrs...)
-
-		var buf bytes.Buffer
-		defer buf.Reset()
-		execTplErrs := exec(parsedT, data, &buf)
-		tplErrs = append(tplErrs, execTplErrs...)
-
-		// outputs html into the textarea, so chrome gets worried
-		// https://stackoverflow.com/a/17815577/2178159
-		w.Header().Add("X-XSS-Protection", "0")
-
-		lines := SplitLines(text)
-		indexTemplate.Execute(w, indexData{
-			RawText:        text,
-			RawData:        rawData,
-			RawFunctions:   rawFunctions,
-			Output:         buf.String(),
-			Errors:         tplErrs,
-			TextLines:      lines,
-			LineNumSpacing: CountDigits(len(lines)),
-		})
-	})
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		indexTemplate.Execute(w, indexData{})
-	})
+	a := &App{index: index}
+	r.Post("/", a.Post)
+	r.Get("/", a.Get)
 
 	log.Printf("starting on port %d\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), r))
+}
+
+func nl() string              { return "\n" }
+func split(s string) []string { return strings.Split(s, ": ") }
+
+func intRange(start, end int) []int {
+	n := end - start + 1
+	result := make([]int, n)
+	for i := 0; i < n; i++ {
+		result[i] = start + i
+	}
+	return result
+}
+
+type App struct {
+	index   *htmlTemplate.Template
+	tplErrs []templateError
+}
+
+var indexDataSamples = []indexData{
+	{
+		RawText: `<!-- https://stackoverflow.com/questions/16734503/access-out-of-loop-value-inside-golang-templates-loop -->
+{{- range .Pages}}
+<li><a href="{{$.Name}}/{{.}}">{{.}}</a></li>
+{{- end}}`,
+		RawData: `{"Name":"兵哥哥", "Pages":["立正","齐步走"]}`,
+	},
+}
+
+func (a *App) Get(w http.ResponseWriter, r *http.Request) {
+	a.tplErrs = make([]templateError, 0)
+
+	for _, v := range indexDataSamples {
+		data := a.createData(v.RawText, v.RawData, v.RawFunctions)
+		w.Header().Add("X-XSS-Protection", "0")
+		if err := a.index.Execute(w, data); err != nil {
+			http.Error(w, fmt.Sprintf("Execute error: %v", err), http.StatusForbidden)
+		}
+		return
+	}
+
+	if err := a.index.Execute(w, indexData{}); err != nil {
+		http.Error(w, fmt.Sprintf("Execute error: %v", err), http.StatusForbidden)
+	}
+}
+
+const maxRequestSize = 32 << 20
+
+func (a *App) Post(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseMultipartForm(maxRequestSize); err != nil {
+		http.Error(w, fmt.Sprintf("ParseMultipartForm error: %v", err), http.StatusForbidden)
+		return
+	}
+
+	a.tplErrs = make([]templateError, 0)
+
+	text, err := getText(r)
+	if err == http.ErrMissingFile {
+		a.tplErrs = append(a.tplErrs, templateError{Line: -1, Char: -1, Description: "couldn't accept file"})
+	} else if err != nil {
+		panic(err)
+	}
+
+	rawData := r.FormValue("data")
+	rawFns := r.FormValue("functions")
+
+	// outputs html into the textarea, so chrome gets worried
+	// https://stackoverflow.com/a/17815577/2178159
+	data := a.createData(text, rawData, rawFns)
+	w.Header().Add("X-XSS-Protection", "0")
+	if err := a.index.Execute(w, data); err != nil {
+		http.Error(w, fmt.Sprintf("Execute error: %v", err), http.StatusForbidden)
+		return
+	}
+}
+
+func (a *App) createData(text, rawData, rawFns string) indexData {
+	var data interface{}
+	if rawData != "" {
+		if err := json.Unmarshal([]byte(rawData), &data); err != nil {
+			a.tplErrs = append(a.tplErrs, templateError{Line: -1, Char: -1, Level: misunderstoodError,
+				Description: fmt.Sprintf("failed to understand data: %v", err)})
+		}
+	}
+
+	t := textTemplate.New("input template")
+
+	// mock template functions - this'll happen automatically as they're found, but errors will be output and there's a max limit
+	var functions []string
+	if rawFns != "" {
+		functions = strings.Split(rawFns, ",")
+	}
+	for _, fn := range functions {
+		fn = strings.TrimSpace(fn)
+		// wrap in func so we can catch panics on bad function names
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					a.tplErrs = append(a.tplErrs, templateError{Line: -1, Char: -1, Level: misunderstoodError,
+						Description: fmt.Sprintf(`bad function name provided: "%s"`, fn)})
+				}
+			}()
+			t = t.Funcs(textTemplate.FuncMap{fn: func() error { return nil }})
+		}()
+	}
+
+	parsedT, parseTplErrs := parse(text, t)
+	a.tplErrs = append(a.tplErrs, parseTplErrs...)
+
+	var buf bytes.Buffer
+	execTplErrs := exec(parsedT, data, &buf)
+	a.tplErrs = append(a.tplErrs, execTplErrs...)
+
+	lines := SplitLines(text)
+	return indexData{
+		RawText:        text,
+		RawData:        rawData,
+		RawFunctions:   rawFns,
+		Output:         buf.String(),
+		Errors:         a.tplErrs,
+		TextLines:      lines,
+		LineNumSpacing: CountDigits(len(lines)),
+	}
 }
--- template.go
+++ template.go
@@ -6,11 +6,12 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
-	textTemplate "text/template"
+	"text/template"
 )
 
+const maxFixes = 10
+
 var (
-	maxFixes                    = 10
 	templateErrorRegex          = regexp.MustCompile(`template: (.*?):((\d+):)?(\d+): (.*)`)
 	findTokenRegex              = regexp.MustCompile(`['"](.+)['"]`)
 	findExprRegex               = regexp.MustCompile(`<(\..+?)>`)
@@ -21,51 +22,40 @@ var (
 
 func createTemplateError(err error, level ErrorLevel) templateError {
 	matches := templateErrorRegex.FindStringSubmatch(err.Error())
-	if len(matches) == 6 {
-		// tplName := matches[1]
+	if len(matches) != 6 {
+		return templateError{Line: -1, Char: -1, Description: err.Error(), Level: misunderstoodError}
+	}
+	// tplName := matches[1]
 
-		// 2 is line + : group if char is found
-		// line is in pos 4, unless a char is found in which case it's 3 and char is 4
+	// 2 is line + : group if char is found
+	// line is in pos 4, unless a char is found in which case it's 3 and char is 4
 
-		lineIndex := 4
-		char := -1
-		if matches[3] != "" {
-			lineIndex = 3
-			char, err = strconv.Atoi(matches[4])
-			if err != nil {
-				char = -1
-			}
-		}
-
-		line, err := strconv.Atoi(matches[lineIndex])
+	lineIndex := 4
+	char := -1
+	if matches[3] != "" {
+		lineIndex = 3
+		char, err = strconv.Atoi(matches[4])
 		if err != nil {
-			line = -1
-		} else {
-			line = line - 1
-		}
-
-		description := matches[5]
-
-		return templateError{
-			Line:        line,
-			Char:        char,
-			Description: description,
-			Level:       level,
+			char = -1
 		}
 	}
-	return templateError{
-		Line:        -1,
-		Char:        -1,
-		Description: err.Error(),
-		Level:       misunderstoodError,
+
+	line, err := strconv.Atoi(matches[lineIndex])
+	if err != nil {
+		line = -1
+	} else {
+		line = line - 1
 	}
+
+	description := matches[5]
+	return templateError{Line: line, Char: char, Description: description, Level: level}
 }
 
-func parse(text string, baseTpl *textTemplate.Template) (*textTemplate.Template, []templateError) {
+func parse(text string, baseTpl *template.Template) (*template.Template, []templateError) {
 	return parseInternal(text, baseTpl, 0)
 }
 
-func parseInternal(text string, baseTpl *textTemplate.Template, depth int) (t *textTemplate.Template, tplErrs []templateError) {
+func parseInternal(text string, baseTpl *template.Template, depth int) (t *template.Template, tplErrs []templateError) {
 	lines := SplitLines(text)
 
 	if depth >= maxFixes {
@@ -73,79 +63,75 @@ func parseInternal(text string, baseTpl *textTemplate.Template, depth int) (t *t
 	}
 
 	t, err := baseTpl.Parse(text)
-	if err != nil {
-		tplErrs = append(tplErrs, createTemplateError(err, parseErrorLevel))
-		// make this mutable
-		tplErr := &tplErrs[len(tplErrs)-1]
+	if err == nil {
+		return t, tplErrs
+	}
 
-		if tplErr.Level != misunderstoodError {
-			if tplErr.Char == -1 {
-				// try to find a character to line up with
-				tokenLoc := findTokenRegex.FindStringIndex(tplErr.Description)
-				if tokenLoc != nil {
-					token := string(tplErr.Description[tokenLoc[0]+1 : tokenLoc[1]-1])
-					lastChar := strings.LastIndex(lines[tplErr.Line], token)
-					firstChar := strings.Index(lines[tplErr.Line], token)
-					// if it's not the only match, we don't know which character is the one the error occured on
-					if lastChar == firstChar {
-						tplErr.Char = firstChar
-					}
+	tplErrs = append(tplErrs, createTemplateError(err, parseErrorLevel))
+	// make this mutable
+	tplErr := &tplErrs[len(tplErrs)-1]
+
+	if tplErr.Level != misunderstoodError {
+		if tplErr.Char == -1 {
+			// try to find a character to line up with
+			tokenLoc := findTokenRegex.FindStringIndex(tplErr.Description)
+			if tokenLoc != nil {
+				token := tplErr.Description[tokenLoc[0]+1 : tokenLoc[1]-1]
+				lastChar := strings.LastIndex(lines[tplErr.Line], token)
+				firstChar := strings.Index(lines[tplErr.Line], token)
+				// if it's not the only match, we don't know which character is the one the error occured on
+				if lastChar == firstChar {
+					tplErr.Char = firstChar
 				}
 			}
+		}
 
-			badFunctionMatch := functionNotFoundRegex.FindStringSubmatch(tplErr.Description)
-			if badFunctionMatch != nil {
-				token := badFunctionMatch[1]
-				t, parseTplErrs := parseInternal(text, baseTpl.Funcs(textTemplate.FuncMap{
-					token: func() error {
-						return nil
-					},
-				}), depth+1)
+		badFunctionMatch := functionNotFoundRegex.FindStringSubmatch(tplErr.Description)
+		if badFunctionMatch != nil {
+			token := badFunctionMatch[1]
+			t, parseTplErrs := parseInternal(text, baseTpl.Funcs(template.FuncMap{
+				token: func() error { return nil },
+			}), depth+1)
+			return t, append(tplErrs, parseTplErrs...)
+		}
+
+		if missingValueForCommandRegex.MatchString(tplErr.Description) {
+			if matches := firstEmptyCommandRegex.FindStringSubmatch(text); matches != nil {
+				line := SplitLines(text)[tplErr.Line]
+				indexes := firstEmptyCommandRegex.FindStringIndex(line)
+				if indexes != nil {
+					tplErr.Char = indexes[0]
+				}
+				replacement := fmt.Sprintf(fmt.Sprintf("%%%ds", len(matches[0])), "")
+				t, parseTplErrs := parseInternal(
+					strings.Replace(text, matches[0], replacement, 1),
+					baseTpl,
+					depth+1,
+				)
 				return t, append(tplErrs, parseTplErrs...)
 			}
-
-			if missingValueForCommandRegex.MatchString(tplErr.Description) {
-				matches := firstEmptyCommandRegex.FindStringSubmatch(text)
-				if matches != nil {
-					line := SplitLines(text)[tplErr.Line]
-					indexes := firstEmptyCommandRegex.FindStringIndex(line)
-					if indexes != nil {
-						tplErr.Char = indexes[0]
-					}
-					replacement := fmt.Sprintf(fmt.Sprintf("%%%ds", len(matches[0])), "")
-					t, parseTplErrs := parseInternal(
-						strings.Replace(text, matches[0], replacement, 1),
-						baseTpl,
-						depth+1,
-					)
-					return t, append(tplErrs, parseTplErrs...)
-				}
-			}
 		}
-
-		return baseTpl, tplErrs
 	}
-	return t, tplErrs
+
+	return baseTpl, tplErrs
 }
 
-func exec(t *textTemplate.Template, data interface{}, buf *bytes.Buffer) []templateError {
-	return execInternal(t, data, buf, 0)
-}
-
-func execInternal(t *textTemplate.Template, data interface{}, buf *bytes.Buffer, depth int) []templateError {
+func exec(t *template.Template, data interface{}, buf *bytes.Buffer) []templateError {
 	tplErrs := make([]templateError, 0)
 	err := t.Execute(buf, data)
-	if err != nil {
-		if err.Error() == fmt.Sprintf(`template: %s: "%s" is an incomplete or empty template`, t.Name(), t.Name()) {
-			return tplErrs
-		}
-		tplErr := createTemplateError(err, execErrorLevel)
-		tplErrs = append(tplErrs, tplErr)
+	if err == nil {
+		return tplErrs
+	}
 
-		matches := findExprRegex.FindStringSubmatch(tplErr.Description)
-		if len(matches) == 2 {
-			fmt.Println(matches[1])
-		}
+	if err.Error() == fmt.Sprintf(`template: %s: "%s" is an incomplete or empty template`, t.Name(), t.Name()) {
+		return tplErrs
+	}
+	tplErr := createTemplateError(err, execErrorLevel)
+	tplErrs = append(tplErrs, tplErr)
+
+	matches := findExprRegex.FindStringSubmatch(tplErr.Description)
+	if len(matches) == 2 {
+		fmt.Println(matches[1])
 	}
 	return tplErrs
 }
